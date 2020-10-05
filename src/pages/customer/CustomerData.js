a472e358988dfcbb45aaba9c5327c60041cc53f4
JeanYaha <jean@yahacomex.com.br>
2020-10-05 08:10:06 -0300

    Correção de render desnecessário ao abrir a tela de dados de cliente

--- src/pages/customer/CustomerData.js
+++ src/pages/customer/CustomerData.js
@@ -1,79 +1,99 @@
-import React, { useEffect } from "react";
+import React from "react";
 import TextField from "@material-ui/core/TextField";
 import callServer from "../../services/callServer";
-import { useTranslation } from "react-i18next";
-import { useInput } from "../../hooks/useInput";
+import { withTranslation } from "react-i18next";
 import Grid from "@material-ui/core/Grid";
 import ModalData from "../../components/modal/ModalData";
 
-const CustomerData = ({ idCustomer, onSave, onClose }) => {
-  const { t } = useTranslation();
-  const errorMessage = t("requiredfield");
+class CustomerData extends React.Component {
+  state = {
+    name: "",
+    errors: [],
+  };
 
-  const {
-    value: name,
-    bind: bindName,
-    setValue: setName,
-    hasErrors: errorsName,
-    setHasErrors: setHasErrorsName,
-  } = useInput("");
+  onChangeForField = (id, newValue) => {
+    this.setState({ [id]: newValue });
+    this.setState({
+      errors: this.state.errors.filter((value) => {
+        return id !== value;
+      }),
+    });
+  };
 
-  const saveData = () => {
+  saveData = () => {
+    const errors = [];
+    const { name } = this.state;
+
     if (name === "") {
-      setHasErrorsName(true);
+      errors.push("name");
+    }
+
+    this.setState({
+      errors: errors,
+    });
+
+    if (errors.length > 0) {
       return;
     }
 
-    onSave({
+    this.props.onSave({
       name,
     });
 
-    onClose();
+    this.props.onClose();
   };
 
-  useEffect(() => {
-    const fetchData = async (idCustomer) => {
-      const response = await callServer.get(`customers/${idCustomer}`);
+  fetchData = (idCustomer) => {
+    callServer.get(`customers/${idCustomer}`).then((response) => {
+      this.setState({
+        name: response.data.name,
+      });
+    });
+  };
 
-      setName(response.data.name);
-    };
+  componentDidMount() {
+    const { idCustomer } = this.props;
 
     if (idCustomer && idCustomer !== -1) {
-      fetchData(idCustomer);
-    } else {
-      setName("");
+      this.fetchData(idCustomer);
     }
+  }
 
-    setHasErrorsName(false);
-  }, [setName, setHasErrorsName, idCustomer]);
+  render() {
+    const { t, onClose } = this.props;
+    const { name, errors } = this.state;
+    const errorMessage = t("requiredfield");
 
-  return (
-    <div>
-      <ModalData
-        onSave={saveData}
-        isOpen={true}
-        onClose={onClose}
-        title="customerdata"
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={12}>
-            <TextField
-              id="name"
-              label={t("name")}
-              variant="outlined"
-              value={name}
-              fullWidth
-              required={true}
-              size="small"
-              {...bindName}
-              error={errorsName}
-              helperText={errorsName && errorMessage}
-            />
+    return (
+      <div>
+        <ModalData
+          onSave={this.saveData}
+          isOpen={true}
+          onClose={onClose}
+          title="customerdata"
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                id="name"
+                label={t("name")}
+                variant="outlined"
+                value={name}
+                fullWidth
+                required={true}
+                size="small"
+                onChange={(e) =>
+                  this.onChangeForField(e.target.id, e.target.value)
+                }
+                error={errors.includes("name")}
+                helperText={errors.includes("name") && errorMessage}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </ModalData>
-    </div>
-  );
-};
+        </ModalData>
+      </div>
+    );
+  }
+}
 
-export default CustomerData;
+export default withTranslation()(CustomerData);
